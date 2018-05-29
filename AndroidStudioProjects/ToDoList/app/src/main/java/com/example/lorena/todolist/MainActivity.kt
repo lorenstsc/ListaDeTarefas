4a5500b7aaa11ebd2ebe27c86deb8681a9299c95
lorenstsc <ltavaressc@gmail.com>
2018-05-29 19:43:24 -0300

    Tarefa segunda nota

---
+++ AndroidStudioProjects/ToDoList/app/src/main/java/com/example/lorena/todolist/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.lorena.todolist
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.main_listview)
+        listView.adapter = MyCustomAdapter(this)
+    }
+    private class MyCustomAdapter(context: Context): BaseAdapter(){
+
+        private val mContext: Context
+
+        private val names = arrayListOf<String>(
+                "Prepare guess list","Order Cake","Buy drinks","Reserve restaurant","Pick up supplies"
+        )
+
+        init {
+            mContext = context
+        }
+
+        override fun getCount(): Int {
+            return names.size
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+            return "NADA"
+        }
+
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val layoutInflater = LayoutInflater.from(mContext)
+            val rowMain = layoutInflater.inflate(R.layout.main_row, viewGroup, false)
+
+            val nameTextView = rowMain.findViewById<TextView>(R.id.name_textView)
+            nameTextView.text = names.get(position)
+
+            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+            positionTextView.text = "Description"
+
+            return rowMain
+
+        }
+    }
+
+}
